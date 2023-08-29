885fc4ff727f5657902602c842776b60950eaa67
Krishna Prabhu Narayana <99739581+Ashish342prabhu@users.noreply.github.com>
2023-08-29 23:00:53 +0530

    Task1.py

--- Level 1 Tasks/Task1.py
+++ Level 1 Tasks/Task1.py
@@ -1,1 +1,10 @@
+mystr=input("Enter the string: ") 
+def reverse_string(mystr):
+    # This function uses slicing to reverse the string. 
+    # The `[::-1]` syntax means to start at the end of the string and move backwards with a step of -1, 
+    # effectively reversing the string.
+    return mystr[::-1]
+revstr=reverse_string(mystr)
+print("string after reversing: ")
+print(revstr) 
 
